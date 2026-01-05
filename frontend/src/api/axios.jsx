916bcd66f0cde80f3db227664df5262750b2ceef
madhu12-c <mchanda2006@gmail.com>
2026-01-05 22:56:25 +0530

    again fixes

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tool-trace.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
